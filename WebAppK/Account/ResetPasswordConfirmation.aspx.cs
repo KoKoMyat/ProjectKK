853261904221e38046bd2bfc7fd1852b25a16d36
Ko Ko <koko@justlogin.com>
2018-02-13 15:07:32 +0800

    Initial mater version
    Initial version

---
+++ WebAppK/Account/ResetPasswordConfirmation.aspx.cs
@@ -0,0 +1,8 @@
+﻿using System.Web.UI;
+
+namespace WebAppK.Account
+{
+    public partial class ResetPasswordConfirmation : Page
+    {
+    }
+}
---
+++ WebAppK/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WebAppK.Startup))]
+namespace WebAppK
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
